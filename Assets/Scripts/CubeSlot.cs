fe80d912d80b5459c855090fd2608321da32ee18
Qu1ck1337 <Glebkraykin@yandex.ru>
2022-07-16 16:06:01 +0300

    Added tool bar

---
+++ Assets/Scripts/CubeSlot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CubeSlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Ondrop");
+        if (eventData.pointerDrag != null)
+        {
+            RectTransform dice = eventData.pointerDrag.GetComponent<RectTransform>();
+            dice.SetParent(transform);
+            dice.anchoredPosition = new Vector2(0f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dice : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private GameObject _panel;
+    [SerializeField] private GameObject _diceHubPanel;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Ondrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDrag");
+        canvasGroup.blocksRaycasts = true;
+
+        if (transform.parent == _panel.transform)
+        transform.SetParent(_diceHubPanel.transform);
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, 0f);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.SetParent(_panel.transform);
+        Debug.Log("Click");
+    }
+}
